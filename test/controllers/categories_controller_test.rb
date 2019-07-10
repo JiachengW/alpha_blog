890928768f9e473f940d985390ecfdfa8d2f9204
Jason Wang <jiacheng_wang@outlook.com>
2019-07-09 23:00:18 -0400

    Simulate login for tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -10,10 +11,18 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@user, "password")
     assert_generates "/categories/new", controller: "categories", action: "new"
   end
 
   test "should get show" do
     assert_generates "/categories/1", { controller: "categories", action: "show", id: @category.id }
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "sports"}}
+    end
+    assert_redirected_to categories_path
   end
 end
